fea3623b876197262d7e5373cc13424ab23d99f5
Xavi <xavier.barker@escapestudios.com>
2025-09-26 07:43:07 +0100

    Refactor main and unit logic for improved structure and functionality; implement cohesive and avoidance behaviors for units.

--- scripts/main.py
+++ scripts/main.py
@@ -4,35 +4,86 @@ import numpy as np
 
 from unit import unit
 
-def initialise():
+def initialise(width, height):
 
-    pygame.init()
+  pygame.init()
 
-    background_colour = (10,15,60)
-    (width, height) = (600, 400)
+  
+
+  screen = pygame.display.set_mode((width, height))
+  pygame.display.set_caption('Boids Demo')
+  
+  return width, height, screen
+
+def genUnits(unitNum, unitList, width, height):
+
+  for i in range(unitNum):
+    unitList.append(unit(i, width, height))
 
 
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Boids Demo')
-    screen.fill(background_colour)
+  return unit
 
-    pygame.display.flip()
+def distVectors(vectors):
+  vectorNum = len(vectors)
+  distList = [[0] * vectorNum for _ in range(vectorNum)]
+  for i in range(vectorNum):
+    distList[i][i] = (0,i)
+    for j in range(vectorNum):
+      if i < j:
+        distance = np.linalg.norm(np.subtract(vectors[i], vectors[j]))
+        distList[i][j] = (distance, j)
+        distList[j][i] = (distance, i)
+
+  return distList
 
 
-    running = True
-
-    while running:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          running = False
 
 def main():
 
-    unitNum = 100
+  (width, height) = (1500, 980)
+  background_colour = (10,15,60)
+  unitNum = 250
+  unitList = []
+  distList = []
+  #seperation Power and Radius
+  sepPow = 0.1
+  sepRad = 25
+  cohPow = .1
+  cohRad = 200
+  aliPow = .1
+  aliRad = 100
 
-    initialise()
+  initParams = initialise(width, height)
+  screen = initParams[2]
 
+  clock = pygame.time.Clock()
+  genUnits(unitNum, unitList, width, height)
+
+
+  while True:
+
+    screen.fill(background_colour)
     
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        exit()
+    
+    distList = distVectors([unit.position for unit in unitList])
+
+    for unit in unitList:
+      unit.cohere(unitList, distList, cohPow, cohRad)
+      unit.avoid(unitList, distList, sepPow, sepRad)
+      unit.align(unitList, distList, aliPow, aliRad)
+      unit.move()
+
+
+    for unit in unitList:
+      unit.display(screen)
+    
+    pygame.display.flip()
+    clock.tick(60)
+
 
 
 main()
--- scripts/unit.py
+++ scripts/unit.py
@@ -25,7 +25,7 @@ class unit:
 
         self.accel = np.array([0,0])
         self.accel_to_cohere = np.array([0, 0])
-        self.accel_to_separate = np.array([0, 0])
+        self.accel_to_avoid = np.array([0, 0])
         self.accel_to_align = np.array([0, 0])
 
         self.poly = np.array([(20, 0), (0, -5), (0, 5)])
@@ -34,16 +34,64 @@ class unit:
 
     def move(self):
 
+        self.accel = self.accel_to_align + self.accel_to_cohere + self.accel_to_avoid
+
+        vector = self.vel + self.accel
+        self.vel = (vector) / np.linalg.norm(vector)
+        self.direction = np.arctan2(self.vel[1], self.vel[0])
         self.position += self.vel
 
         if self.position[0] > self.width or self.position[0] < 0:
-            self.position[0] = np.abs(self.position - self.width)
+            self.position[0] = np.abs(self.position[0] - self.width)
         if self.position[1] > self.height or self.position[1] < 0:
-            self.position[1] = np.abs(self.position - self.height)
+            self.position[1] = np.abs(self.position[1] - self.height)
+
+    def cohere(self, unitList, distance, power, radius):
+        nearUnitsList = [
+            d[1] for d in distance[self.unitID]
+            if 0 < d[0] < radius and unitList[d[1]].colour == self.colour
+            ]
+        if len(nearUnitsList) > 0:
+            nearUnit = [unitList[unitID] for unitID in nearUnitsList]
+            centerOfNear = np.mean(np.array([unit.position for unit in nearUnit]))
+            vector = np.subtract(centerOfNear, self.position)
+
+            self.accel_to_cohere =  power * (vector / np.linalg.norm(vector))
+        else:
+            self.accel_to_cohere = np.array([0,0])
+
+    def avoid(self, unitList, distance, power, radius):
+        nearUnitsList = [
+            d[1] for d in distance[self.unitID]
+            if 0 < d[0] < radius
+        ]
+        if len(nearUnitsList) > 0:
+            nearUnit = [unitList[unitID] for unitID in nearUnitsList]
+            centerOfNear = np.mean(np.array([unit.position for unit in nearUnit]))
+            vector = np.subtract(centerOfNear, self.position)
+
+            self.accel_to_avoid = - power * (vector / np.linalg.norm(vector))
+        else:
+            self.accel_to_avoid = np.array([0,0])
+                      
+    def align(self, unitList, distance, power, radius):
+        nearUnitsList = [
+            d[1] for d in distance[self.unitID]
+            if 0 < d[0] < radius and unitList[d[1]].colour == self.colour
+            ]
+        if len(nearUnitsList) > 0:
+            nearUnit = [unitList[unitID] for unitID in nearUnitsList]
+            vector = np.sum([unit.vel for unit in nearUnit], axis=0)
+
+            self.accel_to_align =  power * (vector / np.linalg.norm(vector))
+        else:
+            self.accel_to_align = np.array([0,0])
+
+
 
     def display(self, screen):
 
-        rotMatrix = np.array([[np.cos(self.direction), - np.sin(self.direction)], [np.sin(self.direction), np.cos(self.direction)]])
+        rotMatrix = np.array([[np.cos(self.direction), -np.sin(self.direction)],[np.sin(self.direction), np.cos(self.direction)]])
         
         rotPoly = np.dot(self.poly, rotMatrix.T) + self.position
 
